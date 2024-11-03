a14eea0efaadeeec953a9186f8301889f3c3a1f3
yamanerkam <erkamyaman35@gmail.com>
2024-11-03 22:54:40 +0300

    sidebar-and-top-added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { SidebarModule } from 'primeng/sidebar';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule,SidebarModule
+  imports: [RouterOutlet, ButtonModule,SidebarModule,MenubarModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'budget-savvy';
-  sidebarVisible : boolean = false;
+  sidebarVisible : boolean = true;
 
   toggleSidebar(){
-    console.log('se')
     this.sidebarVisible = !this.sidebarVisible;
   }
 }
