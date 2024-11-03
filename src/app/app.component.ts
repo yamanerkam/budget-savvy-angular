c84d1e1a158f194340afc45bd8277f3a0ccc6d78
yamanerkam <erkamyaman35@gmail.com>
2024-11-04 02:02:59 +0300

    day-getter-func

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,8 +15,21 @@ import { MenubarModule } from 'primeng/menubar';
 export class AppComponent {
   title = 'budget-savvy';
   sidebarVisible : boolean = true;
+  time : string = this.getTimeOfDay(new Date())
+  userName : string = 'kam'.toLocaleUpperCase();
+  
+  
+  getTimeOfDay(time : Date) {
+    const hour = time.getHours();
+    if (hour >= 5 && hour < 12) {
+        return "Morning";
+    } else if (hour >= 12 && hour < 17) {
+        return "Afternoon";
+    } else if (hour >= 17 && hour < 21) {
+        return "Evening";
+    } else {
+        return "Night";
+    }
+}
 
-  toggleSidebar(){
-    this.sidebarVisible = !this.sidebarVisible;
-  }
 }
