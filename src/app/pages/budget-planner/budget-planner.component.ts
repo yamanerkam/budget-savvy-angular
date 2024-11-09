db4fe4f79e5f8da5ca0ed9de88ef2a6567a819e9
yamanerkam <erkamyaman35@gmail.com>
2024-11-09 19:44:33 +0300

    feature/budget-planer-component-updated
    - option buttons added
    - simple calcualtions are started

--- src/app/pages/budget-planner/budget-planner.component.ts
+++ src/app/pages/budget-planner/budget-planner.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-budget-planner',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule,InputNumberModule,CommonModule,FormsModule],
   templateUrl: './budget-planner.component.html',
   styleUrl: './budget-planner.component.css'
 })
 export class BudgetPlannerComponent {
+  chosenTab : string = 'Needs';
+  rent? : number;
+  groceries? : number;
+  shopping? : number;
+  travel? : number;
+  totalSpent: number = 0;
 
+  changeTab(tab:string){
+    this.chosenTab = tab;
+  } 
+
+  calculateTotalSpent(){
+    console.log('s')
+    this.totalSpent = (this.rent ?? 0) + (this.groceries ?? 0);
+  }
 }
