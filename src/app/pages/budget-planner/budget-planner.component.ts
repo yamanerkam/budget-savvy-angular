219003b95769c538a54f660103c770fa6121f0a5
yamanerkam <erkamyaman35@gmail.com>
2024-11-10 18:53:13 +0300

    feature/budget-planner-component-calulations
    - calculations are set with TypeScript Record<keys, type> in order to prevent spagetti if else hell

--- src/app/pages/budget-planner/budget-planner.component.ts
+++ src/app/pages/budget-planner/budget-planner.component.ts
@@ -17,14 +17,23 @@ export class BudgetPlannerComponent {
   groceries? : number;
   shopping? : number;
   travel? : number;
+  education? : number;
+  emergency? : number;
   totalSpent: number = 0;
 
   changeTab(tab:string){
     this.chosenTab = tab;
   } 
 
-  calculateTotalSpent(){
-    console.log('s')
-    this.totalSpent = (this.rent ?? 0) + (this.groceries ?? 0);
+  private tabCalculations: Record<string, () => number> = {
+    Needs: () => (this.rent ?? 0) + (this.groceries ?? 0),
+    Wants: () => (this.shopping ?? 0) + (this.travel ?? 0),
+    Savings: () => (this.education ?? 0) + (this.emergency ?? 0),
+  };
+
+  calculateTotalSpent(tab: string) {
+    const calculate = this.tabCalculations[tab];
+    this.totalSpent = calculate ? calculate() : 0;
   }
+  
 }
